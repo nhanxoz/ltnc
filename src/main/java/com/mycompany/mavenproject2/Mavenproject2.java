3441df1988cd85692c25048a780f93d499641c7a
nhanxoz <nhan9ckl@gmail.com>
2022-04-28 16:41:08 +0700

    tqerqwer

--- src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -12,6 +12,6 @@ package com.mycompany.mavenproject2;
 public class Mavenproject2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");eqweqwewqe;
     }
 }
